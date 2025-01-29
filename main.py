70087d5807ca2e4c8fffaf60584fff2ac9512f3c
muzudho <muzudho1@gmail.com>
2025-01-29 19:29:38 +0900

    反時計回りに９０°回転

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-########################################
-# MARK: コマンドから実行時
-########################################
+import traceback
 
-if __name__ == '__main__':
-    try:
-        print(f"""横幅、縦幅のあとに図形を続けてください。
+
+def main():
+    # ここから、入力
+    # --------------
+
+    prompt = f"""横幅、縦幅のあとに図形を続けてください。
 例：
 7 5
 ..xxx..
@@ -12,17 +13,81 @@ xxxxx..
 xxxxxxx
 ..xxxxx
 ...xxxx
-""")
+"""
+    lines = input(prompt)
+    width, height = map(int, lines.split(' '))
+
+    pixel_art = []
+
+    for row_number in range(0, height):
         lines = input()
-        width, height = map(int, lines.split(' '))
+        pixel_art.extend(lines)
 
-        pixel_art = []
+    #print(f'{pixel_art=}')
+    print()
+    print_pixel_art(
+            width=width,
+            height=height,
+            pixel_art=pixel_art)
 
-        for row_number in range(0, height):
-            lines = input()
-            pixel_art.append(lines)
+    # ここまで、入力
+    # --------------
 
-        print(f'{pixel_art=}')
+    # 以下、解法
+    # ----------
+
+    # 縦と横のどちらが短いか？
+    WIDTH_IS_SHORTER = 1
+    HEIGHT_IS_SHORTER = 2
+    if height < width:
+        print(f'縦が短い {width=} {height=}')
+        short_side = HEIGHT_IS_SHORTER
+    else:
+        print(f'横が短い {width=} {height=}')
+        short_side = WIDTH_IS_SHORTER
+    
+    if short_side == WIDTH_IS_SHORTER:
+        # 横の方が短いときは、反時計回りに９０°回転させる
+        old_width = width               # 退避
+        old_height = height
+        old_pixel_art = pixel_art
+        new_width = old_height              # ９０°回転
+        new_height = old_width
+        new_pixel_art = list(old_pixel_art)     # シャローコピー
+        for old_y in range(0, old_height):
+            for old_x in range(0, old_width):
+                new_x = old_y
+                new_y = (width-1-old_x)
+                new_pixel_art[new_y * new_width + new_x] = old_pixel_art[old_y * old_width + old_x]
+
+        width = new_width
+        height = new_height
+        pixel_art = new_pixel_art
+
+
+    #print(f'{pixel_art=}')
+    print()
+    print_pixel_art(
+            width=width,
+            height=height,
+            pixel_art=pixel_art)
+
+
+def print_pixel_art(width, height, pixel_art):
+    for y in range(0, height):
+        for x in range(0, width):
+            index = y * width + x
+            print(pixel_art[index], end='')
+        print() # 改行
+
+
+########################################
+# MARK: コマンドから実行時
+########################################
+
+if __name__ == '__main__':
+    try:
+        main()
 
     except Exception as err:
         print(f"""\
